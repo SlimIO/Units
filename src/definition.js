fd8de864226834ea27a694e98dc8ad78ff1065e5
fraxken <gentilhomme.thomas@gmail.com>
2018-10-12 07:30:36 +0200

    Add more stricter checkup on Def class

--- src/definition.js
+++ src/definition.js
@@ -1,3 +1,6 @@
+// Require Third-party Dependencies
+const is = require("@slimio/is");
+
 /**
  * @class UnitDefinition
  * @param {Number} type Unit type
@@ -9,13 +12,25 @@ class UnitDefinition {
 
     /**
      * @constructor
-     * @param {Object=} options options
-     * @param {Number} options.type Unit type
-     * @param {String} options.symbol Unit symbol
-     * @param {Number} options.min min
-     * @param {Number} options.max max
+     * @param {Object} options UnitDefinition options
+     * @param {!Number} options.type Unit type
+     * @param {!String} options.symbol Unit symbol
+     * @param {Number=} options.min min
+     * @param {Number=} options.max max
+     *
+     * @throws {TypeError}
      */
-    constructor(options = Object.create(null)) {
+    constructor(options) {
+        if (!is.plainObject(options)) {
+            throw new TypeError("options should be a plain javascript Object");
+        }
+        if (!is.number(options.type)) {
+            throw new TypeError("options.type should be typeof number");
+        }
+        if (!is.string(options.symbol)) {
+            throw new TypeError("options.symbol should be typeof string");
+        }
+
         this.type = options.type;
         this.symbol = options.symbol;
         this.min = typeof options.min === "number" ? options.min : -Infinity;
