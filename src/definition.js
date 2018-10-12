65848f2e1bc23aa34379b28ec1d09e02d820baec
fraxken <gentilhomme.thomas@gmail.com>
2018-10-12 07:21:03 +0200

    Move Unit class to src/ and rename definition

---
+++ src/definition.js
@@ -0,0 +1,27 @@
+/**
+ * @class Definition
+ * @param {Number} type Unit type
+ * @param {String} symbol Unit symbol
+ * @param {Number} min min
+ * @param {Number} max max
+ */
+class Definition {
+
+    /**
+     * @constructor
+     * @param {Object=} options options
+     * @param {Number} options.type Unit type
+     * @param {String} options.symbol Unit symbol
+     * @param {Number} options.min min
+     * @param {Number} options.max max
+     */
+    constructor(options = Object.create(null)) {
+        this.type = options.type;
+        this.symbol = options.symbol;
+        this.min = typeof options.min === "number" ? options.min : -Infinity;
+        this.max = typeof options.max === "number" ? options.max : Infinity;
+    }
+
+}
+
+module.exports = Definition;
